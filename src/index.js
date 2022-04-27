9815aa8e12ecd6e8ac100093c56a50dbfa9acfe9
mercy-wumi <m.adams1909@gmail.com>
2022-04-27 13:14:58 +0100

    refactor router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from "react-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import UseStateArray from './components/useState-array'
+import UseStateObject from './components/useState-object'
+import UseStateBasics from './components/useState-basics'
+import ErrorExample from './components/error-example'
+
+const root = document.getElementById('root');
+render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='usestatearray' element={<UseStateArray />} />
+          <Route path='usestateobject' element={<UseStateObject />} />
+          <Route path='usestatebasics' element={<UseStateBasics />} />
+          <Route path='errorexample' element={<ErrorExample />} />
+        </Route>
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
