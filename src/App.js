6fd1bc78269d3c193e4783a055d24a56252c0f50
mercy-wumi <m.adams1909@gmail.com>
2022-04-30 17:28:49 +0100

    fixing route issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 import UseStateNav from './components/useState-nav'
-import UseStateArray from './components/useState-array'
-import UseStateObject from './components/useState-object'
-import UseStateBasics from './components/useState-basics'
-import ErrorExample from './components/error-example'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='container'>
-        <UseStateNav />
-      </div>
-      <Routes>
-        <Route path='usestatearray' element={<UseStateArray />} />
-        <Route path='usestateobject' element={<UseStateObject />} />
-        <Route path='usestatebasics' element={<UseStateBasics />} />
-        <Route path='errorexample' element={<ErrorExample />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='container'>
+      <UseStateNav />
+      <Outlet />
+    </div>
   );
 }
 
